6ea385446717fc84750e91cb6d244de0cab8afa6
Hersh Thakur <hersh.thakur@gmail.com>
2023-12-20 10:18:55 +0000

    refactor: using primary constructor

--- UltimateBreakfast/Models/Breakfast.cs
+++ UltimateBreakfast/Models/Breakfast.cs
@@ -1,34 +1,21 @@
 namespace UltimateBreakfast.Models;
 
-public class Breakfast
+public class Breakfast(
+  Guid id,
+  string name,
+  string description,
+  DateTime startDate,
+  DateTime endDate,
+  DateTime lastUpdated,
+  List<string> savory,
+  List<string> sweet)
 {
-  public Guid Id { get; }
-  public string Name { get; }
-  public string Description { get; }
-  public DateTime StartDate { get; }
-  public DateTime EndDate { get; }
-  public DateTime LastUpdated { get; }
-  public List<string> Savory { get; }
-  public List<string> Sweet { get; }
-
-  public Breakfast(
-    Guid id,
-    string name,
-    string description,
-    DateTime startDate,
-    DateTime endDate,
-    DateTime lastUpdated,
-    List<string> savory,
-    List<string> sweet)
-  {
-    Id = id;
-    Name = name;
-    Description = description;
-    StartDate = startDate;
-    EndDate = endDate;
-    LastUpdated = lastUpdated;
-    Savory = savory;
-    Sweet = sweet;
-
-  }
+    public Guid Id { get; } = id;
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public DateTime StartDate { get; } = startDate;
+    public DateTime EndDate { get; } = endDate;
+    public DateTime LastUpdated { get; } = lastUpdated;
+    public List<string> Savory { get; } = savory;
+    public List<string> Sweet { get; } = sweet;
 }
